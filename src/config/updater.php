8e69cfc5b4f9638e3b470d838973a49a5f1b19b5
PawelDmochowski <pawel@dmochowski.co>
2016-10-09 12:17:24 +0200

    default config

---
+++ src/config/updater.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+	'blueprint' => [
+		'created_by' => 'created_by',
+		'updated_by' => 'updated_by',
+		'deleted_by' => 'deleted_by',
+	],
+	'models' => [
+		'created_by' => 'App\User',
+		'updated_by' => 'App\User',
+		'deleted_by' => 'App\User',
+	],
+];
