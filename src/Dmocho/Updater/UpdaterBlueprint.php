530de9a6f20429058532b2e2aa7c77b1e42ad063
PawelDmochowski <pawel@dmochowski.co>
2016-11-19 19:45:40 +0100

    add drops to the blueprint

--- src/Dmocho/Updater/UpdaterBlueprint.php
+++ src/Dmocho/Updater/UpdaterBlueprint.php
@@ -18,4 +18,16 @@ class UpdaterBlueprint extends \Illuminate\Database\Schema\Blueprint
         $this->integer(config('updater.blueprint.deleted_by'))->unsigned()->nullable();
         $this->foreign(config('updater.blueprint.deleted_by'))->references('id')->on('users')->onUpdate('cascade')->onDelete('set null');
     }
+
+    public function dropAuthors() {
+        $this->dropColumn(config('updater.blueprint.created_by'));
+        $this->dropColumn(config('updater.blueprint.updated_by'));
+        $this->dropForeign([config('updater.blueprint.created_by'),config('updater.blueprint.updated_by')]);
+    }
+
+    public function dropSoftDeleters()
+    {
+        $this->dropColumn(config('updater.blueprint.deleted_by'));
+        $this->dropForeign([config('updater.blueprint.deleted_by')]);
+    }
 }
