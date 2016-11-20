32c3673a1015248fa7ea2b44a0475a63cac19908
PawelDmochowski <pawel@dmochowski.co>
2016-11-20 18:20:49 +0100

    fix blueprint drop order

--- src/Dmocho/Updater/UpdaterBlueprint.php
+++ src/Dmocho/Updater/UpdaterBlueprint.php
@@ -20,14 +20,14 @@ class UpdaterBlueprint extends \Illuminate\Database\Schema\Blueprint
     }
 
     public function dropAuthors() {
+        $this->dropForeign([config('updater.blueprint.created_by'),config('updater.blueprint.updated_by')]);
         $this->dropColumn(config('updater.blueprint.created_by'));
         $this->dropColumn(config('updater.blueprint.updated_by'));
-        $this->dropForeign([config('updater.blueprint.created_by'),config('updater.blueprint.updated_by')]);
     }
 
     public function dropSoftDeleters()
     {
-        $this->dropColumn(config('updater.blueprint.deleted_by'));
         $this->dropForeign([config('updater.blueprint.deleted_by')]);
+        $this->dropColumn(config('updater.blueprint.deleted_by'));
     }
 }
