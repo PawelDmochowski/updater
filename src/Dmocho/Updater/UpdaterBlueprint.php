d4390f4f20f49154a044161570871d6c62f8830e
PawelDmochowski <pawel@dmochowski.co>
2017-05-22 11:57:14 +0200

    fix migrations

--- src/Dmocho/Updater/UpdaterBlueprint.php
+++ src/Dmocho/Updater/UpdaterBlueprint.php
@@ -19,8 +19,10 @@ class UpdaterBlueprint extends \Illuminate\Database\Schema\Blueprint
         $this->foreign(config('updater.blueprint.deleted_by'))->references('id')->on('users')->onUpdate('cascade')->onDelete('set null');
     }
 
-    public function dropAuthors() {
-        $this->dropForeign([config('updater.blueprint.created_by'),config('updater.blueprint.updated_by')]);
+    public function dropAuthors()
+    {
+        $this->dropForeign([config('updater.blueprint.created_by')]);
+        $this->dropForeign([config('updater.blueprint.updated_by')]);
         $this->dropColumn(config('updater.blueprint.created_by'));
         $this->dropColumn(config('updater.blueprint.updated_by'));
     }
